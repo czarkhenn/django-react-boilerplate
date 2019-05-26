64c5d00c4ad8d678df3a208701a249ccaf351ff5
czarkhenn <czarkhenn@gmail.com>
2019-05-26 13:54:07 +0800

    added is_student

---
+++ users/serializers.py
@@ -0,0 +1,18 @@
+from rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'useraname', 'password', 'is_student', 'is_teacher')
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    is_student = serializers.BooleanField()
+    is_teacher = serializers.BooleanField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'useraname', 'password', 'is_student', 'is_teacher')
